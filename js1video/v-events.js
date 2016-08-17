3df010fc34c77b3c27fa71a6aa8ff5ef539c2f18
Your Name <you@example.com>
2016-08-17 08:12:31 +0300

    1105170816 vista

---
+++ js1video/v-events.js
@@ -0,0 +1,32 @@
+//v-events.js
+
+
+// http://www.newthinktank.com/2015/09/learn-javascript-one-video/
+// https://www.youtube.com/watch?v=fju9ii8YsGs
+   
+
+ 
+ 
+//44:01  line 394    
+ 
+ 
+document.write("Events :  <br />");
+ 
+ 
+  
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+
--- js1video/v-fun.js
+++ js1video/v-fun.js
@@ -48,4 +48,69 @@ function multiply(func, num){
 document.write("3 * 15 = ", multiply(times3, 15), "<br />");
  
 
+// Define a function expression
+// We can assign functions to variables, store them in arrays,
+// pass them into other functions and return them from functions
+var triple = function(num){
+  return num * 3;
+};
+ 
+document.write("3 * 45 = ", multiply(triple, 45), "<br />")
+ 
+// Receive variable number of arguments
+function getSum(){
+  var sum = 0;
+  for(i = 0; i < arguments.length; i++){
+    sum += arguments[i];
+  }
+  return sum;
+}
+ 
+document.write("Sum : ", getSum(1,2,3,4,5), "<br />");
+
+
+// Return a variable number of values
+function times2(theArray){
+ 
+  var newArray = [];
+  for(i = 0; i < theArray.length; i++){
+    newArray.push(theArray[i] * 2);
+  }
+  return newArray;
+}
+ 
+document.write("Array Doubled : ", times2([1,2,3,4,5]).toString(), "<br />");
+ 
+// Recursive Function
+function factorial(num){
+  if(num <= 1){
+    return 1;
+  } else {
+    return num * factorial(num - 1);
+  }
+}
+ 
+document.write("Factorial of 4 : ", factorial(4), "<br />");
+ 
+// 1st: num = 4 * factorial(3) = 4 * 6 = 24
+// 2nd: num = 3 * factorial(2) = 3 * 2 = 6
+// 3rd: num = 2 * factorial(1) = 2 * 1 = 2
+ 
+ 
+ 
+ 
+//44:01  line 394 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
 
