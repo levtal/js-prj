91bc471aaf6bf527b3b8bf1edbbe7804fc76bd09
root <root@localhost.localdomain>
2016-08-17 14:41:47 +0300

    events 1445170816

--- js1video/v-events.js
+++ js1video/v-events.js
@@ -10,14 +10,39 @@
 //44:01  line 394    
  
  
-document.write("Events :  <br />");
+document.write("<b>EVENT HANDLING </b>  <br />");
  
+  
+// ---------- EVENT HANDLING ----------
+function openAlert(mess){
+  alert(mess);
+}
+ 
+ 
+// ---------- DATE ----------
+// Get a Date object
+var curDate = new Date();
+ 
+document.write("Date : ", curDate.getDate(), "<br />");
+document.write("Month : ", curDate.getMonth(), "<br />");
+document.write("Day : ", curDate.getDay(), "<br />");
+document.write("Year : ", curDate.getFullYear(), "<br />");
+document.write("Time : ", curDate.getHours(), ":", curDate.getMinutes(),
+  ":", curDate.getSeconds(), ":", curDate.getMilliseconds(), "<br />");
+// Create a Date object for my birthday
+var myBD = new Date("December 21, 2015");
+ 
+var msForBD = myBD.getTime();
+var timeNow = curDate.getTime();
+var tilMyBD = msForBD - timeNow;
+ 
+document.write("Days til Birthday : ", tilMyBD / (1000 * 60 * 60 * 24), "<br />");
  
   
  
  
  
- 
+ //51:40  line 463
  
  
  
