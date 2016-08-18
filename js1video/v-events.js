61e1494093085dd453ecd679301a351292013d4a
Your Name <you@example.com>
2016-08-18 08:47:03 +0300

    vista 1147180816

--- js1video/v-events.js
+++ js1video/v-events.js
@@ -68,11 +68,76 @@ function getChar(event) {
  
  
  //51:40  line 463
+ // Change text when the input gains focus
+document.getElementById('charInput').onfocus = function(event) {
+  document.getElementById('keyData').innerHTML = "Input Gained Focus";
+}
+ 
+// Change text when the input loses focus
+document.getElementById('charInput').onblur = function(event) {
+  document.getElementById('keyData').innerHTML = "Input Lost Focus";
+}
+ 
+// Change text when text is selected
+document.getElementById('charInput').onselect = function(event) {
+  document.getElementById('keyData').innerHTML = "Text Selected";
+}
+ 
+// Add a listener that triggers a function on browser resize
+window.addEventListener("resize", browserResized);
+ 
+function browserResized() {
+  document.getElementById('keyData').innerHTML = "I've been resized";
+}
+ 
+// Make image invisible on click
+document.getElementById('logo').onclick = function(event) {
+ 
+  // Change the class for the image
+  document.getElementById('logo').className = "hidden";
+ 
+  // Change the input element value
+  document.getElementById('mouseInput').value = "Clicked on image with button " + event.button;
+}
+ 
+// Make image visible on click
+document.getElementById('logoButton').onclick = function(event) {
+  document.getElementById('logo').className = "show";
+}
  
  
  
+ // Change image src on mouseover
+document.getElementById('logo').onmouseover = function(event) {
+  document.getElementById('logo').src = "ntt-logo-horz.png";
+  document.getElementById('mouseInput').value = "Mouse Over image";
+}
  
+// Change image src back on mouseout
+document.getElementById('logo').onmouseout = function(event) {
+  document.getElementById('logo').src = "ntt-logo.png";
+  document.getElementById('mouseInput').value = "Mouse Left image";
+}
  
+// Get mouse x y coordinates
+document.body.onmousemove = function(e) {
+    e = e || window.event;
+ 
+    // Get pageX, pageY : Mouse position relative to the html doc
+    var pageX = e.pageX;
+    var pageY = e.pageY;
+    if (pageX === undefined) {
+ 
+      // clientX, clientY : Mouse position relative to the browsers viewport
+      // scrollLeft, scrollTop : Pixels an element is scrolled left or
+      // from the top
+    pageX=e.clientX+document.body.scrollLeft+document.documentElement.scrollLeft;
+       pageY = e.clientY + document.body.scrollTop + document.documentElement.scrollTop;
+    }
+ 
+    document.getElementById('mouseX').value = pageX;
+    document.getElementById('mouseY').value = pageY;
+};
  
  
  
