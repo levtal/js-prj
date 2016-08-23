c1684be3e08f56589dbc36cf5a0549b32bd08044
Your Name <you@example.com>
2016-08-23 16:33:47 +0300

    jquery

---
+++ jquery/jq.js
@@ -0,0 +1,27 @@
+//http://www.newthinktank.com/2015/10/learn-jquery-one-video/
+
+//https://www.youtube.com/watch?v=BWXggB-T1jQ
+
+//https://developer.mozilla.org/en-US/docs/Web/CSS
+
+$("document").ready(function() { // line 108
+
+    // ADDING / REMOVING / CHANGING ELEMENTS
+ 
+  // You can change CSS attributes like this
+  // # targets ids and . targets classes
+  $("#wrapper").css("width", 500);
+  $("#wrapper").css("margin", "auto");
+  $("#logo2").css("float", "left");
+  $("#p_two").css("color", "red");
+  
+  // How to pass in multiple arguments
+  $("#p_two").css({
+	  "background" : "url('repeatBkgrnd.png') repeat-y"
+	  });
+ 
+   // line 122  6:58
+  
+  
+  
+}); <!-- End of JQuery Code -->
