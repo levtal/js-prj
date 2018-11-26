eaa271a4942cf56c960c65814e64da9a5ec0f782
Richm <ric5hme45@yahoo.com>
2018-11-26 15:27:03 +0200

    bmk

---
+++ bookmark/js/main.js
@@ -0,0 +1,2 @@
+ alert(11.7)
+ //https://www.youtube.com/watch?v=DIVfDZZeGxM&index=2&list=PLillGF-RfqbbnEGy3ROiLWk7JMCuSyQtX
---
+++ p1/js/main.js
@@ -0,0 +1,24 @@
+ 
+     
+     //var  colors = ['red','blue','yellow']
+	 //alert(colors.length)
+	 //document.getElementById("d").innerHTML = colors.length
+	 //var fruits = ["Banana", "Orange", "Apple", "Mango"];
+     // document.getElementById("d").innerHTML = fruits.toString();
+	  
+	  var text = "";
+var i;
+for (i = 0; i < 5; i++) {
+    text += "The number is " + i + "<br>";
+	console.log(i);
+}
+
+//document.getElementById("d2").innerHTML = text;
+
+function doClick(id){
+	document.getElementById("h").innerHTML = "fruits.toString();" 
+	id.innerHTML = "ffg";
+	var time = document.getElementById("time")
+	time.innerHTML = Date();
+	alert("dfgg");
+}
