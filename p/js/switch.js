6d8412be44d0d5b3beb4a812f907395d038c6ad0
Your Name <you@example.com>
2018-11-28 19:24:35 +0200

    fff

---
+++ p/js/switch.js
@@ -0,0 +1,16 @@
+  a = 8 + 2;
+
+switch (a) {
+  case 4:
+    alert('Right!');
+    break;
+
+  case 3:                    // (*) grouped two cases
+  case 5:
+    alert('Wrong!');
+    alert("Why don't you take a math class?");
+    break;
+
+  default:
+    alert('The result is strange. Really.');
+}
