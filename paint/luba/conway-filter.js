a9bba202abee76f58a0c4062360d6c893633b49a
Richm <ric5hme45@yahoo.com>
2019-06-29 16:55:15 +0300

    color

--- paint/luba/conway-filter.js
+++ paint/luba/conway-filter.js
@@ -6,7 +6,47 @@
 // Conway's Game of Life
 //------------------------------------------
 
+
+ const LIFE_VALUE = 450;
+ const DEAD=0,LIVE=1 ;
 var Lubfilt = {};
+var color_arr = Array.from(Array(9), () => new Array(2));
+color_arr[0][DEAD] = [ 0,0,220];
+color_arr[1][DEAD] = [ 0,0,220];
+color_arr[2][DEAD] = [ 0,0,220];
+color_arr[3][DEAD] = [ 0,0,220];
+color_arr[4][DEAD] = [ 0,0,220];
+color_arr[5][DEAD] = [ 0,0,220];
+color_arr[6][DEAD] = [ 0,0,220];
+color_arr[7][DEAD] = [ 0,0,220];
+color_arr[8][DEAD] = [ 0,0,220];
+color_arr[0][LIVE] = [ 220,0,0];
+color_arr[1][LIVE] = [ 220,0,0];
+color_arr[2][LIVE] = [ 220,0,0];
+color_arr[3][LIVE] = [ 220,0,0];
+color_arr[4][LIVE] = [ 220,0,0];
+color_arr[5][LIVE] = [ 220,0,0];
+color_arr[6][LIVE] = [ 220,0,0];;
+color_arr[6][LIVE] = [ 220,0,0];
+color_arr[7][LIVE] = [ 220,0,0];
+color_arr[8][LIVE] = [ 220,0,0];
+//console.info(arr);
+
+function Create2DArray(rows,columns) {
+   var arr = [];
+   columns = 2*columns;
+   for(var x = 0; x <= rows; x++){
+        arr[x] = [];
+        for(var y = 0; y <=columns; y++){
+           arr[x][y] = [ 0,0,220];
+    }
+}
+return arr;
+}
+
+
+
+
 
 
 Lubfilt.getPix = function(pixels,x,y){
@@ -57,20 +97,7 @@ Lubfilt.countNeig = function(pixels,x,y){
 
 
 
-  function Create2DArray(rows,columns) {
-     var arr = [];
-     columns = 2*columns;
-     for(var x = 0; x <= rows; x++){
-          arr[x] = [];
-          for(var y = 0; y <=columns; y++){
-             arr[x][y] = [ 0,0,220];
-      }
-  }
-  return arr;
- }
 
- const LIFE_VALUE = 250;
- const DEAD=0,LIVE=1 ;
  Lubfilt.changeLiveingCell = function(neig_counter,color) {
    // this cell is alive (life_value >= LIFE_VALUE)
    //change it acording to neig number
@@ -78,14 +105,13 @@ Lubfilt.countNeig = function(pixels,x,y){
         case 0:
         case 1: //return color;  break;
         case 2:
-        case 3:
-        case 4:return [220, 0, 0];   break;
+        case 3:return color;  break;
+        case 4:return (color_arr[neig_counter][LIVE]);   break;
         case 5:return color;  break;
-       
-  	   case 6:
-        case 7:
-        case 8:  // return [0,150,150];   break;
-      default: return [150,150,150];   break;
+
+  	    case 6:
+        case 7: return color;  break;
+        case 8:   return [150,150,150];   break;
 
     }
  }//changeLiveingCell
@@ -94,17 +120,17 @@ Lubfilt.countNeig = function(pixels,x,y){
        // this cell is dead (life_value< LIFE_VALUE)
        //change it acording to neig number
      switch (neig_counter) {
-         case 0: return [0,220, 0];   break;
+         case 0:return color;   break;
          case 1:
          case 3: //return [40,80,0];   break;
          case 2:
-         case 3: //return [80,130,0];   break;
-         case 4:
+         case 3:
+         case 4:return (color_arr[neig_counter][DEAD]);   break;
          case 5:
          case 6: //return [120,200,0];   break;
 
          case 7:
-         case 8: return [0,0,220];   break;
+         case 8: return [0,180,0];   break;
             //return [0,0,255];  break;
         //default: return [250,250,250];   break;
      }
@@ -153,10 +179,11 @@ Lubfilt.countNeig = function(pixels,x,y){
         color = Lubfilt.calculateColor (imgPixels,x,y);
 
         if (life_value >= LIFE_VALUE){
-            temp_image_pixels[x][y] =  Lubfilt.changeLiveingCell(neig_counter,color);
-
+           temp_image_pixels[x][y] =  Lubfilt.changeLiveingCell(neig_counter,color);
+            //  temp_image_pixels[x][y] =  color_arr[neig_counter][LIVE]
           }else{
-              temp_image_pixels[x][y] =  Lubfilt.changeDeadCell(neig_counter,color);
+             temp_image_pixels[x][y] =  Lubfilt.changeDeadCell(neig_counter,color);
+            //temp_image_pixels[x][y] =  color_arr[neig_counter][DEAD]
            }
 
        /*kill alive
